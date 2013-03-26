30b0c69f94ce51fd71df1ab6a3b48c9815dae24a
bradleslie <brad.leslie@townsuite.com>
2013-03-26 14:59:00 -0300

    a

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -3,5 +3,14 @@
 
     Private Sub thisisit()
 
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Did this in Branch 1
+        MsgBox("Bug 1")
     End Sub
 End Class
